fada7ead5d2a5523d4be664a4403ea2a199eda58
nya-suke <hiromochiokui@gmail.com>
2021-08-15 18:59:42 +0900

    ユーザー登録機能実装

--- app/controllers/contributions_controller.rb
+++ app/controllers/contributions_controller.rb
@@ -1,4 +1,7 @@
 class ContributionsController < ApplicationController
+  before_action :set_contribution, only: [:edit, :show]
+  before_action :move_to_index, except: [:index, :show]
+
   def index
     @contributions = Contribution.all
   end
@@ -17,7 +20,6 @@ class ContributionsController < ApplicationController
   end
 
   def edit
-    @contribution = Contribution.find(params[:id])
   end
 
   def update
@@ -25,9 +27,22 @@ class ContributionsController < ApplicationController
     contribution.update(contribution_params)
   end
 
+  def show
+  end
+
   private
   def contribution_params
     params.require(:contribution).permit(:title, :text)
+  end
+
+  def set_contribution
+    @contribution = Contribution.find(params[:id])
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
   end
 
 
